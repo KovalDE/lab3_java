ee5fdb09620479cb3b6d44ba92f36f97abb47148
KovalDE <72768859+KovalDE@users.noreply.github.com>
2021-12-06 09:23:39 +0200

    Add files via upload

--- Task1/Main.java
+++ Task1/Main.java
@@ -3,7 +3,7 @@ package Task1;
 public class Main {
     public static void main(final String[] args) {
         try {
-            final User user1 = new User("Dima", "Koval", 30, null, "null");
+            final User user1 = new User("155", "Koval", 30, null, "null");
         } catch (MyException e) {
             System.out.println(e.getMessages()+", "+e.getErrorCode());
         }
